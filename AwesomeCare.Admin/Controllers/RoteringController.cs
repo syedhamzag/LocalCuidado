ee4d0c9c5024b0b4a369b68aad139dcee9129695
Olamide Akinnagbe <Olamide.Akinnagbe@sterlingbankng.com>
2019-11-29 08:44:38 +0100

    added rotering page

--- AwesomeCare.Admin/Controllers/RoteringController.cs
+++ AwesomeCare.Admin/Controllers/RoteringController.cs
@@ -6,8 +6,11 @@ using AwesomeCare.Admin.Services.ClientRota;
 using AwesomeCare.Admin.Services.ClientRotaType;
 using AwesomeCare.Admin.Services.RotaTask;
 using AwesomeCare.Admin.ViewModels.Rotering;
+using AwesomeCare.DataTransferObject.DTOs.ClientRotaType;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using AwesomeCare.Admin.Extensions;
 
 namespace AwesomeCare.Admin.Controllers
 {
@@ -16,7 +19,7 @@ namespace AwesomeCare.Admin.Controllers
         IClientRotaTypeService _clientRotaTypeService;
         IClientRotaService _clientRotaService;
         IRotaTaskService _rotaTaskService;
-        public RoteringController(IRotaTaskService rotaTaskService,IClientRotaTypeService clientRotaTypeService, IClientRotaService clientRotaService)
+        public RoteringController(IRotaTaskService rotaTaskService, IClientRotaTypeService clientRotaTypeService, IClientRotaService clientRotaService)
         {
             _clientRotaTypeService = clientRotaTypeService;
             _clientRotaService = clientRotaService;
@@ -24,14 +27,54 @@ namespace AwesomeCare.Admin.Controllers
         }
         public async Task<IActionResult> Index()
         {
-            RoteringViewModel model =new RoteringViewModel();
+            RoteringViewModel model = new RoteringViewModel();
             var rotaTypes = await _clientRotaTypeService.Get();
             var rotas = await _clientRotaService.Get();
             var rotaTasks = await _rotaTaskService.Get();
             model.Rotas = rotas.Select(r => new SelectListItem { Text = r.RotaName, Value = r.RotaId.ToString() }).ToList();
             model.RotaTasks = rotaTasks.Select(r => new SelectListItem { Text = r.TaskName, Value = r.RotaTaskId.ToString() }).ToList();
             model.RotaTypes = rotaTypes;
+            HttpContext.Session.Set<List<string>>("weekDays", model.WeekDays);
+            HttpContext.Session.Set<List<GetClientRotaType>>("rotaTypes", rotaTypes);
             return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Index(IFormCollection formsCollection)
+        {
+            List<GetClientRotaType> rotaTypes = HttpContext.Session.Get<List<GetClientRotaType>>("rotaTypes");
+            List<string> weekDays = HttpContext.Session.Get<List<string>>("weekDays");
+
+            foreach (var rotaType in rotaTypes)
+            {
+                var rotatype = formsCollection[rotaType.RotaType];
+                if (rotatype.Count > 0 && rotatype[0].ToString().Equals("on", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    foreach (var weekDay in weekDays)
+                    {
+                        string rotaId = $"{rotaType.RotaType}-{weekDay}-rota";
+                        string tastId = $"{rotaType.RotaType}-{weekDay}-rotaTask";
+                        string startTimeId = $"{rotaType.RotaType}-{weekDay}-StartTime";
+                        string stopTimeId = $"{rotaType.RotaType}-{weekDay}-StopTime";
+
+                        var rota = formsCollection[rotaId];
+                        var task = formsCollection[tastId];
+                        var startTime = formsCollection[startTimeId];
+                        var stopTime = formsCollection[stopTimeId];
+
+                        //Count the number of Task
+                        for (int i = 0; i < task.Count; i++)
+                        {
+
+                        }
+
+                    }
+                }
+
+            }
+
+            return RedirectToAction("Index");
         }
     }
 }
