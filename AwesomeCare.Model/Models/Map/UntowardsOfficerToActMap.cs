80411236d7cc34d4104f80fae30974311f5604e6
Olamide Akinnagbe <Olamide.Akinnagbe@sterlingbankng.com>
2020-03-04 10:40:05 +0100

    added Untowards

---
+++ AwesomeCare.Model/Models/Map/UntowardsOfficerToActMap.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AwesomeCare.Model.Models.Map
+{
+    public class UntowardsOfficerToActMap : IEntityTypeConfiguration<UntowardsOfficerToAct>
+    {
+        public void Configure(EntityTypeBuilder<UntowardsOfficerToAct> builder)
+        {
+            builder.ToTable("tbl_UntowardsOfficerToAct");
+            builder.HasKey(k => k.UntowardsOfficerToActId);
+
+            #region Properties
+            builder.Property(p => p.UntowardsOfficerToActId)
+               .HasColumnName("UntowardsOfficerToActId")
+               .IsRequired();
+
+            builder.Property(p => p.StaffPersonalInfoId)
+              .HasColumnName("StaffPersonalInfoId")
+              .IsRequired();
+
+            builder.Property(p => p.UntowardsId)
+             .HasColumnName("UntowardsId")
+             .IsRequired();
+
+            #endregion
+        }
+    }
+}
---
+++ AwesomeCare.Model/Models/UntowardsOfficertoAct.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AwesomeCare.Model.Models
+{
+  public  class UntowardsOfficerToAct
+    {
+        public int UntowardsOfficerToActId { get; set; }
+        public int StaffPersonalInfoId { get; set; }
+        public int UntowardsId { get; set; }
+
+
+        public virtual Untowards Untowards { get; set; }
+    }
+}
