93c645503dde3dba2428bc0dc3a823ec5caa3187
Olamide Akinnagbe <Olamide.Akinnagbe@sterlingbankng.com>
2020-05-24 22:55:44 +0100

    working on communication

---
+++ AwesomeCare.DataTransferObject/Enums/Enumeration.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace AwesomeCare.DataTransferObject.Enums
+{
+    public abstract class Enumeration : IComparable
+    {
+        public string Name { get; private set; }
+        public int Id { get; private set; }
+
+        public Enumeration(int id,string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public override string ToString() => Name;
+
+        public static IEnumerable<T> GetAll<T>() where T : Enumeration
+        {
+            var fields = typeof(T).GetFields(BindingFlags.Public | BindingFlags.Static | BindingFlags.DeclaredOnly);
+
+            return fields.Select(t => t.GetValue(null)).Cast<T>();
+        }
+
+        public override bool Equals(object obj)
+        {
+            var otherObj = obj as Enumeration;
+            
+            if (otherObj == null)
+                return false;
+
+            var typeMatches = GetType().Equals(obj.GetType());
+            var valueMatches = Id.Equals(otherObj.Id);
+
+            return typeMatches && valueMatches;
+        }
+        public int CompareTo(object obj) => Id.CompareTo(((Enumeration)obj).Id);
+        
+    }
+}
---
+++ AwesomeCare.Model/Models/Communication.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AwesomeCare.Model.Models
+{
+    public class Communication
+    {
+        public int CommunicationId { get; set; }
+        public int From { get; set; }
+        public int To { get; set; }
+        public string Message { get; set; }
+    }
+}
