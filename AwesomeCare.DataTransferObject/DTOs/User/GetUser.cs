749da7078fd00aa9ea298cde80b8ad913778fd59
Akinnagbe <olamidejames007@gmail.com>
2021-04-09 14:13:49 +0100

    added Edit Applicationuser

---
+++ AwesomeCare.DataTransferObject/DTOs/User/GetUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AwesomeCare.DataTransferObject.DTOs.User
+{
+   public class GetUser
+    {
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public bool EmailConfirmed { get; set; }
+        public bool LockedOutEnabled { get; set; }
+
+    }
+}
