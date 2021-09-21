50c7d8fc89ecc392fd7b14f3ab2cc447c06812cf
Syed Hamza Ghazanfar <ghazanfar.cc@gmail.com>
2021-09-21 13:32:20 +0000

    Updated Startup.cs

--- AwesomeCare.Admin/Startup.cs
+++ AwesomeCare.Admin/Startup.cs
@@ -103,7 +103,6 @@ using AwesomeCare.Admin.Services.InterestAndObjective;
 using AwesomeCare.Admin.Services.Pets;
 using AwesomeCare.Admin.Services.TaskBoard;
 using AwesomeCare.Admin.Services.HospitalEntry;
-using AwesomeCare.Admin.Services.HospitalExit;
 
 namespace AwesomeCare.Admin
 {
@@ -944,12 +943,6 @@ namespace AwesomeCare.Admin
             {
                 c.BaseAddress = new Uri(uri);
             }).AddTypedClient(r => RestService.For<IHospitalEntryService>(r))
-            .AddHttpMessageHandler<AuthenticatedHttpClientHandler>();
-
-            services.AddHttpClient("hospitalexit", c =>
-            {
-                c.BaseAddress = new Uri(uri);
-            }).AddTypedClient(r => RestService.For<IHospitalExitServices>(r))
             .AddHttpMessageHandler<AuthenticatedHttpClientHandler>();
         }
     }
